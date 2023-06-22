56d9bf5c956df8337bd2b19f52744a27167dfecd
hagan23 <haganemmanuel23@gmail.com>
2023-06-22 14:44:53 +0000

    Added remove  book

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Book from './Book';
 
-const BooksList = ({ books }) => (
-  <ul>
-    {books.map(({
-      id, title, author, category,
-    }) => (
-      <Book key={id} id={id} title={title} author={author} category={category} />
-    ))}
-  </ul>
-);
+const BooksList = ({ books }) => {
+  const handleRemove = (id) => {
+    // Handle the removal of the book with the given id
+    // You can pass this function as a prop to the Book component
+  };
+
+  return (
+    <ul>
+      {books.map(({ id, title, author, category }) => (
+        <li key={id}>
+          <Book
+            id={id}
+            title={title}
+            author={author}
+            category={category}
+            onRemove={() => handleRemove(id)}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 BooksList.propTypes = {
-  books: PropTypes.arrayOf.isRequired,
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
 export default BooksList;
