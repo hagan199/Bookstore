fe7e5215a0185b87f2b77e3c45a10285f3388d49
hagan23 <haganemmanuel23@gmail.com>
2023-06-29 08:11:11 +0000

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
-import categoriesReducer from './categories/categories';
+import categoryReducer from './categories/categories';
+import uiSlice from './ui/uiSlice';
 
+// Configure the Redux store
 const store = configureStore({
-  reducer: { books: booksReducer, categories: categoriesReducer },
+  reducer: {
+    // Set up reducers for different slices of the state
+    books: booksReducer, // Reducer for books slice
+    categories: categoryReducer, // Reducer for categories slice
+    ui: uiSlice, // Reducer for UI slice
+  },
 });
 
 export default store;
